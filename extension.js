69adb6ea67193bf6c947e6bc995d7b983a27aac8
CAP_DEV <127816113+capapiluk@users.noreply.github.com>
2026-02-18 14:57:33 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -8,116 +8,125 @@
     color: "#00C7B7",
 
     blocks: [
-        {   // เชื่อมต่อ
+        // ===== ตั้งค่าครั้งแรก (Setup) =====
+        {   // 1. เชื่อมต่อ WiFi (ESP32)
+            xml: `<block type="blynk_wifi_connect">
+                    <value name="ssid"><shadow type="text"><field name="TEXT">YourWiFi</field></shadow></value>
+                    <value name="password"><shadow type="text"><field name="TEXT">YourPassword</field></shadow></value>
+                  </block>`
+        },
+        {   // 2. Template ID
+            xml: `<block type="blynk_template_id">
+                    <value name="template_id"><shadow type="text"><field name="TEXT">TMPL000000</field></shadow></value>
+                  </block>`
+        },
+        {   // 3. ชื่อ Device
+            xml: `<block type="blynk_device_name">
+                    <value name="name"><shadow type="text"><field name="TEXT">My Device</field></shadow></value>
+                  </block>`
+        },
+        {   // 4. Firmware Version
+            xml: `<block type="blynk_firmware_version">
+                    <value name="version"><shadow type="text"><field name="TEXT">1.0.0</field></shadow></value>
+                  </block>`
+        },
+        {   // 5. เชื่อมต่อ Blynk
             xml: `<block type="blynk_init">
                     <value name="auth"><shadow type="text"><field name="TEXT">YourAuthToken</field></shadow></value>
                     <value name="server"><shadow type="text"><field name="TEXT">blynk.cloud</field></shadow></value>
                     <value name="port"><shadow type="math_number"><field name="NUM">80</field></shadow></value>
                   </block>`
         },
-        {   // Run
+        
+        // ===== ใช้งานหลัก (Main) =====
+        {   // 6. Blynk Run (ต้องมี!)
             xml: `<block type="blynk_run"></block>`
         },
-        {   // Virtual Write
+        {   // 7. ส่งค่าไป App
             xml: `<block type="blynk_virtual_write">
                     <value name="pin"><shadow type="math_number"><field name="NUM">1</field></shadow></value>
                     <value name="value"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
                   </block>`
         },
-        {   // handle_event read
-            xml: `<block type="blynk_handle_read">
+        {   // 8. ขอค่าจาก App
+            xml: `<block type="blynk_sync_virtual">
                     <value name="pin"><shadow type="math_number"><field name="NUM">1</field></shadow></value>
                   </block>`
         },
-        {   // handle_event write
+        {   // 9. เช็คสถานะ
+            xml: `<block type="blynk_is_connected"></block>`
+        },
+        
+        // ===== รับข้อมูล (Events) =====
+        {   // 10. เมื่อ App เขียนค่า
             xml: `<block type="blynk_handle_write">
                     <field name="VAR">values</field>
                     <value name="pin"><shadow type="math_number"><field name="NUM">1</field></shadow></value>
                   </block>`
         },
-        {   // handle connect
+        {   // 11. เมื่อ App อ่านค่า
+            xml: `<block type="blynk_handle_read">
+                    <value name="pin"><shadow type="math_number"><field name="NUM">1</field></shadow></value>
+                  </block>`
+        },
+        {   // 12. เมื่อเชื่อมต่อ
             xml: `<block type="blynk_handle_connect"></block>`
         },
-        {   // handle disconnect
+        {   // 13. เมื่อขาดการเชื่อมต่อ
             xml: `<block type="blynk_handle_disconnect"></block>`
         },
-        {   // sync_virtual
-            xml: `<block type="blynk_sync_virtual">
-                    <value name="pin"><shadow type="math_number"><field name="NUM">1</field></shadow></value>
+        {   // 14. ดึงค่าจาก values
+            xml: `<block type="blynk_get_value">
+                    <value name="values"><shadow type="variables_get"><field name="VAR">values</field></shadow></value>
+                    <value name="index"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
                   </block>`
         },
-        {   // set_property
-            xml: `<block type="blynk_set_property">
-                    <value name="pin"><shadow type="math_number"><field name="NUM">1</field></shadow></value>
-                    <value name="prop"><shadow type="text"><field name="TEXT">color</field></shadow></value>
-                    <value name="value"><shadow type="text"><field name="TEXT">#FF0000</field></shadow></value>
-                  </block>`
-        },
-        {   // notify
+        
+        // ===== การแจ้งเตือน (Notifications) =====
+        {   // 15. Push Notification
             xml: `<block type="blynk_notify">
                     <value name="message"><shadow type="text"><field name="TEXT">Alert!</field></shadow></value>
                   </block>`
         },
-        {   // email
+        {   // 16. ส่ง Email
             xml: `<block type="blynk_email">
                     <value name="to"><shadow type="text"><field name="TEXT">you@email.com</field></shadow></value>
                     <value name="subject"><shadow type="text"><field name="TEXT">Subject</field></shadow></value>
                     <value name="body"><shadow type="text"><field name="TEXT">Message body</field></shadow></value>
                   </block>`
         },
-        {   // log_event
+        {   // 17. บันทึก Event
             xml: `<block type="blynk_log_event">
                     <value name="event_name"><shadow type="text"><field name="TEXT">sensor_alert</field></shadow></value>
                     <value name="description"><shadow type="text"><field name="TEXT">Value out of range</field></shadow></value>
                   </block>`
         },
-        {   // is_connected
-            xml: `<block type="blynk_is_connected"></block>`
-        },
-        {   // get_value
-            xml: `<block type="blynk_get_value">
-                    <value name="values"><shadow type="variables_get"><field name="VAR">values</field></shadow></value>
-                    <value name="index"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+        
+        // ===== ขั้นสูง (Advanced) =====
+        {   // 18. ตั้งค่า Widget
+            xml: `<block type="blynk_set_property">
+                    <value name="pin"><shadow type="math_number"><field name="NUM">1</field></shadow></value>
+                    <value name="prop"><shadow type="text"><field name="TEXT">color</field></shadow></value>
+                    <value name="value"><shadow type="text"><field name="TEXT">#FF0000</field></shadow></value>
                   </block>`
         },
-        {   // template_id
-            xml: `<block type="blynk_template_id">
-                    <value name="template_id"><shadow type="text"><field name="TEXT">TMPL000000</field></shadow></value>
-                  </block>`
-        },
-        {   // device_name
-            xml: `<block type="blynk_device_name">
-                    <value name="name"><shadow type="text"><field name="TEXT">My Device</field></shadow></value>
-                  </block>`
-        },
-        {   // firmware_version
-            xml: `<block type="blynk_firmware_version">
-                    <value name="version"><shadow type="text"><field name="TEXT">1.0.0</field></shadow></value>
-                  </block>`
-        },
-        {   // update_property  
+        {   // 19. อัพเดต Property
             xml: `<block type="blynk_update_property">
                     <value name="pin"><shadow type="math_number"><field name="NUM">1</field></shadow></value>
                     <value name="property"><shadow type="text"><field name="TEXT">color</field></shadow></value>
                     <value name="value"><shadow type="text"><field name="TEXT">#FF0000</field></shadow></value>
                   </block>`
         },
-        {   // batch_send_start
+        {   // 20. เริ่มส่งหลายค่า
             xml: `<block type="blynk_batch_start"></block>`
         },
-        {   // batch_send_end 
+        {   // 21. จบการส่งหลายค่า
             xml: `<block type="blynk_batch_end"></block>`
         },
-        {   // wifi_connect (ESP32)
-            xml: `<block type="blynk_wifi_connect">
-                    <value name="ssid"><shadow type="text"><field name="TEXT">YourWiFi</field></shadow></value>
-                    <value name="password"><shadow type="text"><field name="TEXT">YourPassword</field></shadow></value>
-                  </block>`
-        },
-        {   // wifi_info (ESP32)
+        {   // 22. ข้อมูล WiFi
             xml: `<block type="blynk_wifi_info"></block>`
         },
-        {   // memory_info (ESP32)
+        {   // 23. ข้อมูล Memory
             xml: `<block type="blynk_memory_info"></block>`
         }
     ],
